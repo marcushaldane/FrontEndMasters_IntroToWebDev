646a2c46dc62becccc41a43562075e5c7023e4e6
marcushaldane <mrhaldane2017@gmail.com>
2024-04-21 00:57:57 -0500

    wordGame files are functionally the same as the previous commit. Adding new asynchronous files to work on callbacks, promises, await and fetch functionality concepts before finishing wordGame.

--- wordGame.js
+++ wordGame.js
@@ -18,32 +18,56 @@ function changeInnerText (guessID, inputID, keyPress) {
     document.getElementById(guessID).getElementsByClassName(inputID)[0].innerHTML = keyPress;
 }
 
-function constructGuessString () {
+function constructGuessString (guessID) {
     console.log(`constructGuessString`);
-    let guessString = ``;
+    let guessString = ``, elementsClassName;
+    for( let i = 0; i < 5; i++) {
+        elementsClassName = `input-` + (i+1);
+        guessString += document.getElementById(guessID).getElementsByClassName(elementsClassName)[0].innerHTML;
+        console.log(guessString);
+    }
     return guessString;
 }
 
-function validateSubmission () {
-    console.log(`validateSubmission`);
-}
+// function validateSubmission () {
+//     console.log(`validateSubmission`);
+// }
 
-function submitGuess (userGuess) {
-    // validateSubmission(); 
-    console.log(`submitGuess`);
-}
+// function submitGuess (wotd, userGuess) {
+//     // validateSubmission(); 
+//     console.log(`submitGuess: ${wotd}`);
+// }
 
 function loseState () {
     alert(`you lose`);
 }
 
+
+// async function getWordOfTheDay () {
+//     const wotdURL = `https://words.dev-apis.com/word-of-the-day/`;
+//     try {
+//         const response = await fetch(wotdURL);
+//         if (!response.ok) {
+//             throw new Error(`could not fetch resource`);
+//         }
+//         const processedResponse = await response.json();
+//         const word = processedResponse.word;
+//         submitGuess(word, `test`);
+//         return word;
+//     } catch(error) {
+//         console.error(error);
+//     }
+// }
+
 function init () {
-    // document.querySelector(`.input-1`).focus();
+    // let wotd;// = 
+    // getWordOfTheDay().then((word) => {console.log(`wotd: ${word}`)});// {wotd = word;})
+    // console.log(`1.) wotd: ${wotd}`);
+    
     let guessNum = 1;
     let inputNum = 1;
 
     window.addEventListener(`keydown`, (e) => {
-        
         let keyPress = e.key;
         // console.log(`e.key: ${keyPress}`);
         let inputID = `input-` + inputNum;
@@ -63,19 +87,20 @@ function init () {
             } 
             changeInnerText(guessID, inputID, ``);
         } else if (isEnter(keyPress)) {
-            
             let userGuess = constructGuessString(guessID);
-            // submit word
-            submitGuess(userGuess);
-            guessNum++; // move focus to next row
-            if (guessNum > 6) { 
-                loseState(); // TODO: finish loseState()
-                // TODO: add reset state
+            if (userGuess.length == 5) {
+                // submitGuess(wotd, userGuess);
+                guessNum++; // move focus to next row
+                if (guessNum > 6) { 
+                    loseState(); // TODO: finish loseState()
+                    // TODO: add reset state
+                }
+                inputNum = 1;
+                // console.log(`guessNum: ${guessNum}`);
+            } else {
+                // alert(`Guess Too Short!`)
             }
-            inputNum = 1;
-            // console.log(`guessNum: ${guessNum}`);
-        }
-        
+        }  
     })
 }
 init();
